d2d8cf9d427ac710099fb0091ef83c37e9b5b731
Andrey Gubanov <a@odessite.com.ua>
2015-12-03 18:44:48 +0200

    minor

--- src/js/app/main.class.js
+++ src/js/app/main.class.js
@@ -104,11 +104,14 @@ export default class Main extends MK.Object {
 
 		this.loading = false;
 
-		setTimeout(() => {
-			let el = document.querySelector('.demo .in');
-			el.focus();
-			el.selectionStart = el.selectionEnd = el.value.length;
-		});
+		if(location.hash == '#!home') {
+			setTimeout(() => {
+				let el = document.querySelector('.demo .in');
+				el.focus();
+				el.selectionStart = el.selectionEnd = el.value.length;
+			});
+		}
+
 	}
 
 	initDynamicStyles() {
