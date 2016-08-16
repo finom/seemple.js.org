a38dc0c3f1b9cc02b0e568f089b14159d056c7a4
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-08-16 21:21:59 +0300

    clean up before work under the next version

--- assets/starter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
-then
-        export PATH=/usr/local/bin:$PATH
-        forever start --sourceDir /home/finom/web/matreshka/ app.js >> /home/finom/web/matreshka/log.txt 2>&1
-fi
