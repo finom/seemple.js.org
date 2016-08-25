ab772b9041d19e44c015bf1a5a1268e7e449a481
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-08-25 11:11:18 +0300

    use bool var instead of switcher var in display and className binders

--- doc/en/Matreshka.binders/Matreshka.binders.className.jsdoc.js
+++ doc/en/Matreshka.binders/Matreshka.binders.className.jsdoc.js
@@ -5,7 +5,7 @@
 @since 0.1
 @summary Returns a binder which switches over DOM node class name depending on instance property value. If property value equals ``true`` non-strictly, a class name is added, otherwise - it's removed. The logic can be changed by passing ``false`` as the second argument and in this way, a class name will be added  when a property value equals ``false`` non-strictly and vice versa.
 @param {string} className
-@param {boolean} [switcher=true]
+@param {boolean} [bool=true]
 @returns {binder}
 @example
 this.bindNode('myKey', '.my-element',
--- doc/en/Matreshka.binders/Matreshka.binders.display.jsdoc.js
+++ doc/en/Matreshka.binders/Matreshka.binders.display.jsdoc.js
@@ -4,7 +4,7 @@
 @importance 2
 @since 0.1
 @summary Returns a binder which controls a visibility of DOM node (using ``style.display``) depending on the instance property value
-@param {boolean} [switcher=true] - If the argument equals ``true``, a node is hidden when a property value is falsy; if it equals ``false``, it is hidden when a property value is truly.
+@param {boolean} [bool=true] - If the argument equals ``true``, a node is hidden when a property value is falsy; if it equals ``false``, it is hidden when a property value is truly.
 @returns {binder}
 @example
 this.bindNode('myKey', '.my-element', Matreshka.binders.display(true));
